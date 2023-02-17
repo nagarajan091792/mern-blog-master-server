aaa14f10a696b5d9c962ab6546cb810f68ebcf5a
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2023-02-18 01:30:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,orgin: "*"}));
+app.use(cors({credentials:true,orgin: "https://roaring-lolly-990f40.netlify.app"}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
