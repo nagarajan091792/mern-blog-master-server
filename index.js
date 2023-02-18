df97294df389d7ebc30f6d18c317ab9469d1e52b
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2023-02-18 13:29:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
 
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, async (err,info) => {
-    if (err) throw err;
+//     if (err) throw err;
     const {title,summary,content} = req.body;
     const postDoc = await Post.create({
       title,
