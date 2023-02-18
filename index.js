0323f97ccd05d306474ef94505c3988aa430bb70
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2023-02-18 09:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:false,orgin: "*"}));
+const corsConfig = {
+    credentials: true,
+    origin: true,
+};
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
